e59446cfa20b97a06fdadc535a742a0a27fa290c
raviqqe <raviqqe@gmail.com>
2015-05-08 13:22:50 +0000

    document root can be configured by a command line argument

--- src/main.rb
+++ src/main.rb
@@ -2,8 +2,17 @@
 
 require 'webrick'
 
+DOC_ROOT = ARGV[0] || "."
+DEBUG = true
+
+def debug(x)
+  puts x if DEBUG
+end
+
+debug("DOC_ROOT = #{DOC_ROOT}")
+
 s = WEBrick::HTTPServer.new(:Port => 8000,
-    :DocumentRoot => ".")
+    :DocumentRoot => DOC_ROOT)
 trap("INT") {s.shutdown}
 
 s.start
