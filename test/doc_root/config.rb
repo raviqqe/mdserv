a870532ac7c76db73bc85d49ea6fa4f3ec7535b5
raviqqe <raviqqe@gmail.com>
2015-05-09 06:34:29 +0000

    sample dir polished

--- test/doc_root/config.rb
+++ test/doc_root/config.rb
@@ -1,5 +1,8 @@
 module Config
   PORT = 8000
+  TITLE = "my homepage | this is test"
+  CSS = "http://github.com/sindresorhus/github-markdown-css"\
+      "/raw/master/github-markdown.css"
   FOOTER = "copy.html"
   INVALID_EXTS = [".html", ".htm", ".rb"]
 end
