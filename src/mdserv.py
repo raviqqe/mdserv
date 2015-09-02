ca292a2cc926f7b602d93630fadbcb1b7f24e58b
raviqqe <raviqqe@gmail.com>
2015-09-03 04:47:37 +0900

    Fix re

--- src/mdserv.py
+++ src/mdserv.py
@@ -299,7 +299,8 @@ class HTMLTableOfContents(HTMLElem):
 class HTMLNavigation(HTMLElem):
   def __init__(self, parent_directory_path):
     self.text = '<p><a href="{}">back</a></p><hr/>' \
-                .format(re.match(r".*[^/]", parent_directory_path).group(0))
+                .format(re.match(r"(/)|(.*[^/])",
+                        parent_directory_path).group(0))
 
 
 class HTML:
